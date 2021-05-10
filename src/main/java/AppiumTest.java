ec2d0a29067891effd251980c4f7c1ce3e9db144
sravanalucky <sravana.lucky@gmail.com>
2021-05-10 19:22:30 +0800

    first commit

---
+++ src/main/java/AppiumTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+    @Test
+    public void NativeAPPAndroid()
+    {
+        System.out.println("NativeAPPAndroid");
+
+    }
+    @Test
+    public void IOSApps()
+    {
+        System.out.println("IOSApps");
+    }
+}
---
+++ src/main/java/SeleniumTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+    @Test
+    public void BrowserAutomation()
+    {
+        System.out.println("BrowserAutomation");
+    }
+    @Test
+    public void elementsUI()
+    {
+        System.out.println("elementsUI");
+    }
+}
